9c24f82dc7cf0b8ac14365e12d58477d1f39a704
Sergey Nazhestkin <9497020@mail.ru>
2022-01-28 13:11:45 +0300

    Создание новых объектов связанных данных.

--- SportsStore.Web/Pages/EditorPageModel.cs
+++ SportsStore.Web/Pages/EditorPageModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SportsStore.Web.EF;
 using SportsStore.Web.Models;
@@ -21,5 +22,16 @@ namespace SportsStore.Web.Pages
 
         public IEnumerable<Supplier> Suppliers => DataContext.Suppliers;
 
+        protected async Task CheckNewCategory(Product product)
+        {
+            if (product.CategoryId == -1 && !string.IsNullOrEmpty(product.Category?.Name))
+            {
+                DataContext.Categories.Add(product.Category);
+                await DataContext.SaveChangesAsync();
+                product.CategoryId = product.Category.CategoryId;
+                ModelState.Clear();
+                TryValidateModel(product);
+            }
+        }
     }
 }
